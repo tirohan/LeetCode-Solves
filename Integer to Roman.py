7fa9578654730b500cb050b5e74a2e9cba134d5c
tirohan <tanbinislam009@gmail.com>
2022-07-24 18:14:23 +0600

    codes added

---
+++ Integer to Roman.py
@@ -0,0 +1,27 @@
+class Solution:
+    def intToRoman(self, num: int) -> str:
+        """
+        :type num: int
+        :rtype: str
+        """
+        mapping = [(1000, 'M'),
+                    (900, 'CM'),
+                    (500, 'D'),
+                    (400, 'CD'),
+                    (100, 'C'),
+                    (90, 'XC'),
+                    (50, 'L'),
+                    (40, 'XL'),
+                    (10, 'X'),
+                    (9, 'IX'),
+                    (5, 'V'),
+                    (4, 'IV'),
+                    (1, 'I'),]
+
+        roman = []
+        for i, numeral in mapping:
+            while num >= i:
+                num -= i
+                roman.append(numeral)
+
+        return "".join(roman)
