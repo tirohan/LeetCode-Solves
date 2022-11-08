230fea13597bfe26a9e2f7d6566a1ee3548e1a02
Tanbin_Islam_Rohan <34601845+tirohan@users.noreply.github.com>
2022-11-09 01:24:46 +0600

    code added

---
+++ Make The String Great.py
@@ -0,0 +1,10 @@
+class Solution:
+    def makeGood(self, s: str) -> str:
+        stk = []
+        for ch in s:
+            counter_ch = ch.upper() if ch.islower() else ch.lower()
+            if stk and stk[-1] == counter_ch:
+                stk.pop()
+            else:
+                stk.append(ch)
+        return "".join(stk)
