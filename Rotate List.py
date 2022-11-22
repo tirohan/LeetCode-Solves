5b85e957db44e311f13cab85c3ade0f6652a4ec6
tirohan <tanbinislam009@gmail.com>
2022-11-22 12:35:12 +0600

    code added

---
+++ Rotate List.py
@@ -0,0 +1,42 @@
+class ListNode:
+    def __init__(self, x):
+        self.val = x
+        self.next = None
+
+    def __repr__(self):
+        if self:
+            return "{} -> {}".format(self.val, repr(self.next))
+
+
+class Solution(object):
+    def rotateRight(self, head, k):
+        """
+        :type head: ListNode
+        :type k: int
+        :rtype: ListNode
+        """
+        if not head or not head.next:
+            return head
+
+        n, cur = 1, head
+        while cur.next:
+            cur = cur.next
+            n += 1
+        cur.next = head
+
+        cur, tail = head, cur
+        for _ in range(n - k % n):
+            tail = cur
+            cur = cur.next
+        tail.next = None
+
+        return cur
+
+
+if __name__ == "__main__":
+    head = ListNode(1)
+    head.next = ListNode(2)
+    head.next.next = ListNode(3)
+    head.next.next.next = ListNode(4)
+    head.next.next.next.next = ListNode(5)
+    print(Solution().rotateRight(head, 2))
