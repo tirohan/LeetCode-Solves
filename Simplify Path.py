d649a4ee64fed70c0858b440bdb5031e878824ae
Tanbin_Islam_Rohan <34601845+tirohan@users.noreply.github.com>
2022-12-01 23:12:30 +0600

    code added

---
+++ Simplify Path.py
@@ -0,0 +1,15 @@
+class Solution:
+    # @param path, a string
+    # @return a string
+    def simplifyPath(self, path):
+        stack, tokens = [], path.split("/")
+        for token in tokens:
+            if token == ".." and stack:
+                stack.pop()
+            elif token != ".." and token != "." and token:
+                stack.append(token)
+        return "/" + "/".join(stack)
+
+if __name__ == "__main__":
+    print(Solution().simplifyPath("/../"))
+    print(Solution().simplifyPath("/home//foo/"))
