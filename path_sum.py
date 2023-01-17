445ab2f8c2476450b045c66180efbbe3fe3dce2c
Tanbin_Islam_Rohan <34601845+tirohan@users.noreply.github.com>
2023-01-17 23:44:32 +0600

    code added

---
+++ path_sum.py
@@ -0,0 +1,6 @@
+class Solution:
+    def hasPathSum(self, root: TreeNode, sum: int) -> bool:
+            if root is None: return False
+            if root.left is None and root.right is None and sum == root.val:
+                return True
+            return self.hasPathSum(root.left, sum-root.val) or self.hasPathSum(root.right, sum-root.val)
