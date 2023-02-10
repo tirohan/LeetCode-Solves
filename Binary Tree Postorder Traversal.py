c8a7554417a2a26d8ee58b02ea8fa61990e56337
tirohan <tanbinislam009@gmail.com>
2023-02-10 23:32:26 +0600

    code added

---
+++ Binary Tree Postorder Traversal.py
@@ -0,0 +1,11 @@
+class Solution:
+    def postorderTraversal(self, root: Optional[TreeNode]) -> List[int]:
+        res = []
+        stack = [root]
+        while stack:
+            node = stack.pop()
+            if node:
+                res.append(node.val)
+                stack.append(node.left)
+                stack.append(node.right)
+        return res[::-1]
